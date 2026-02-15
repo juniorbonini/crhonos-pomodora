22c80d069902e1feabbe6fd542a793b98b4fc6bc
juniorbonini <juniorboninia7x@gmail.com>
2026-02-15 19:20:10 -0300

    feat(button): finalize styles, variants, sizes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,14 @@
 import styles from "./style.module.css";
 import type { ButtonProps } from "../../types/Button/button";
 
-export function Button({ children, size, variant }: ButtonProps) {
+export function Button({ children, size, variant, onClick }: ButtonProps) {
   return (
-    <button className={`${styles.button} ${styles[size]} ${styles[variant]}`}>
+    <button
+      onClick={onClick}
+      className={`${styles.button} ${styles[size]} ${styles[variant]}`}
+    >
       {children}
+      <span className={styles.rippleContainer} />
     </button>
   );
 }
