27828877a2751614f5f8efb4d9d41191fd43d00d
juniorbonini <juniorboninia7x@gmail.com>
2026-02-16 17:42:30 -0300

    feat(settings): persist user settings via context dispatch

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,10 +1,36 @@
+import { useRef, type ChangeEvent } from "react";
 import { Button } from "../../components/Button";
 import { Container } from "../../components/Container";
 import { Heading } from "../../components/Heading";
 import { Input } from "../../components/Input";
 import { MainTemplate } from "../../templates/Main";
+import { useCycleContext } from "../../context/CycleContext/cycle-context";
+import { CycleActionTypes } from "../../context/CycleReducer/cycle-action";
+import { showNotification } from "../../models/Notifications/show-notification";
 
 export function Settings() {
+  const { dispatch } = useCycleContext();
+  const inputWorkTime = useRef(null);
+  const inputshortBreakTime = useRef(null);
+  const inputLongBreakTime = useRef(null);
+
+  function handleSaveChanges(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const workTime = Number(inputWorkTime.current?.value);
+    const shortBreakTime = Number(inputshortBreakTime.current?.value);
+    const longBreakTime = Number(inputLongBreakTime.current?.value);
+
+    dispatch({
+      type: CycleActionTypes.CHANGE,
+      payload: {
+        workTime,
+        shortBreakTime,
+        longBreakTime,
+      },
+    });
+
+    showNotification.success("As configurações foram salvas");
+  }
   return (
     <MainTemplate>
       <Container>
@@ -16,15 +42,28 @@ export function Settings() {
           descanso longo.
         </p>
       </Container>
-      <form style={{ display: "flex", flexDirection: "column", gap: "3.2rem" }}>
+      <form
+        style={{ display: "flex", flexDirection: "column", gap: "3.2rem" }}
+        onSubmit={handleSaveChanges}
+      >
         <div>
-          <Input type="number" label="Foco (min)" id="workTime" />
+          <Input
+            type="number"
+            label="Foco (min)"
+            id="workTime"
+            min={25}
+            defaultValue={25}
+            ref={inputWorkTime}
+          />
         </div>
         <div>
           <Input
             type="number"
             label="Descanso curto (min)"
             id="shortBreakTime"
+            min={5}
+            defaultValue={5}
+            ref={inputshortBreakTime}
           />
         </div>
         <div>
@@ -32,6 +71,9 @@ export function Settings() {
             type="number"
             label="Descanso longo (min)"
             id="longBreakTime"
+            min={15}
+            defaultValue={15}
+            ref={inputLongBreakTime}
           />
         </div>
         <Container>
