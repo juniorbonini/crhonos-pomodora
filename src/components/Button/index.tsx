1c28c41f097ea55f3f0c316fc2661d56161493e0
juniorbonini <juniorboninia7x@gmail.com>
2026-02-15 21:41:57 -0300

    style(ui): refine button and menu components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,12 +3,14 @@ import type { ButtonProps } from "../../types/Button/button";
 
 export function Button({ children, size, variant, onClick }: ButtonProps) {
   return (
-    <button
-      onClick={onClick}
-      className={`${styles.button} ${styles[size]} ${styles[variant]}`}
-    >
-      {children}
-      <span className={styles.rippleContainer} />
-    </button>
+    <div className={styles.container}>
+      <button
+        onClick={onClick}
+        className={`${styles.button} ${styles[size]} ${styles[variant]}`}
+      >
+        {children}
+        <span className={styles.rippleContainer} />
+      </button>
+    </div>
   );
 }
--- src/templates/Main/index.tsx
+++ src/templates/Main/index.tsx
@@ -1,4 +1,7 @@
+import { FloppyDiskBackIcon } from "@phosphor-icons/react";
+import { Button } from "../../components/Button";
 import { Container } from "../../components/Container";
+import { Cycle } from "../../components/Cycle";
 import { Input } from "../../components/Input";
 import { Menu } from "../../components/Menu";
 import { TimerDisplay } from "../../components/TimerDisplay";
@@ -14,6 +17,12 @@ export function MainTemplate() {
       </Container>
       <Container>
         <TimerDisplay />
+      </Container>
+      <Container>
+        <Cycle />
+        <Button size="md">
+          <FloppyDiskBackIcon />
+        </Button>
       </Container>
     </>
   );
