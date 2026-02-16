12ab00513daf4574653a9e607aa8957b35b649c6
juniorbonini <juniorboninia7x@gmail.com>
2026-02-16 12:43:15 -0300

    feat(workers): add timer manager worker and timer worker

---
+++ src/workers/TimerManagerWorker/timer-manager-worker.ts
@@ -0,0 +1,30 @@
+import type { CycleState } from "../../models/Cycles/cycle-state";
+
+let instance: TimerWorkerManager | null = null;
+
+export class TimerWorkerManager {
+  private worker: Worker;
+  private constructor() {
+    this.worker = new Worker(new URL("../TimerWorker/timer-worker.js"));
+  }
+
+  static getInstance() {
+    if (!instance) {
+      instance = new TimerWorkerManager();
+    }
+    return instance;
+  }
+
+  postMessage(message: CycleState) {
+    this.worker.postMessage(message);
+  }
+
+  onmessage(cb: (e: MessageEvent) => void) {
+    this.worker.onmessage = cb;
+  }
+
+  terminate() {
+    this.worker.terminate();
+    instance = null;
+  }
+}
---
+++ src/workers/TimerWorker/timer-worker.js
@@ -0,0 +1,18 @@
+let isRunning = false;
+
+self.onmessage = function (event) {
+  const state = event.data;
+  const { activeCycle, secondsRemaining } = state;
+  const endDate = activeCycle + secondsRemaining * 1000;
+
+  function tick() {
+    const now = new Date.now();
+    const countDownSeconds = Math.floor((endDate - now) / 1000);
+
+    self.postMessage(countDownSeconds);
+
+    setTimeout(tick, 1000);
+  }
+
+  tick();
+};
