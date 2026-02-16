8e45c7063d62d5157294671f555951028d7c85c9
juniorbonini <juniorboninia7x@gmail.com>
2026-02-16 17:28:05 -0300

    refactor: simple refactor on button component and history page finalized

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import styles from "./style.module.css";
 
 import { TrashIcon } from "@phosphor-icons/react";
@@ -8,54 +9,130 @@ import { Heading } from "../../components/Heading";
 import { useCycleContext } from "../../context/CycleContext/cycle-context";
 import { formatDate } from "../../utils/formatDate/format-date";
 import { getCycleStatus } from "../../utils/cycleStatus/cycle-status";
-import { tr } from "date-fns/locale";
+import { useEffect, useState } from "react";
+import type { SortCycleProps } from "../../types/SortCycle/sort-cycle";
+import { sortCycles } from "../../utils/sortCycle/sort-cycle";
+import { CycleActionTypes } from "../../context/CycleReducer/cycle-action";
+import { showNotification } from "../../models/Notifications/show-notification";
 
 export function History() {
-  const { state } = useCycleContext();
-  const cycleType = {};
+  const { state, dispatch } = useCycleContext();
+  const [confirmClearHistory, setConfirmClearHistory] = useState(false);
+  const hasCycles = state.cycles.length > 0;
+  const [sortCycleOptions, setSortCycleOptions] = useState<SortCycleProps>(
+    () => {
+      return {
+        cycles: sortCycles({ cycles: state.cycles }),
+        field: "startDate",
+        direction: "desc",
+      };
+    },
+  );
+  useEffect(() => {
+    setSortCycleOptions((prev) => ({
+      ...prev,
+      fiedl: prev.field,
+      direction: prev.direction,
+    }));
+  }, [state.cycles]);
+
+  useEffect(() => {
+    if (!confirmClearHistory) return;
+    setConfirmClearHistory(false);
+
+    dispatch({ type: CycleActionTypes.RESET });
+  }, [confirmClearHistory, dispatch]);
+
+  useEffect(() => {
+    return () => {
+      showNotification.dimiss();
+    };
+  }, []);
+
+  function handleSortCycle({ field }: Pick<SortCycleProps, "field">) {
+    const newDirection = sortCycleOptions.direction === "desc" ? "asc" : "desc";
+
+    setSortCycleOptions({
+      cycles: sortCycles({
+        direction: newDirection,
+        field,
+        cycles: sortCycleOptions.cycles,
+      }),
+      direction: newDirection,
+      field,
+    });
+  }
+
+  function handleResetHistory() {
+    showNotification.dimiss();
+    showNotification.confirm("Tem certeza?", (confirmation) =>
+      setConfirmClearHistory(confirmation),
+    );
+  }
   return (
     <MainTemplate>
       <Container>
         <div className={styles.container}>
           <Heading>Histórico</Heading>
-          <Button size="sm" variant="danger">
+          <Button size="sm" variant="danger" onClick={handleResetHistory}>
             <TrashIcon />
           </Button>
         </div>
       </Container>
 
       <Container>
-        <div className={styles.table}>
-          <table>
-            <thead>
-              <tr>
-                <th className={styles.thSort}>Tarefa ↕</th>
-                <th className={styles.thSort}>Duração ↕</th>
-                <th className={styles.thSort}>Data ↕</th>
-                <th className={styles.thSort}>Status</th>
-                <th className={styles.thSort}>Tipo</th>
-              </tr>
-            </thead>
-            <tbody>
-              {state.cycles.map((cycle) => {
-                const cycleType = {
-                  workTime: "Foco",
-                  shortBreakTime: "Descanso curto",
-                  longBreakTime: "Descanso longo",
-                };
-                return (
-                  <tr key={cycle.id}>
-                    <td>{cycle.name}</td>
-                    <td>{cycle.duration}</td>
-                    <td>{formatDate(cycle.startDate)}</td>
-                    <td>{getCycleStatus(cycle, state.activeCycle)}</td>
-                    <td>{cycleType[cycle.type]}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+        {hasCycles ? (
+          <div className={styles.table}>
+            <table>
+              <thead>
+                <tr>
+                  <th
+                    className={styles.thSort}
+                    onClick={() => handleSortCycle({ field: "name" })}
+                  >
+                    Tarefa ↕
+                  </th>
+                  <th
+                    className={styles.thSort}
+                    onClick={() => handleSortCycle({ field: "duration" })}
+                  >
+                    Duração ↕
+                  </th>
+                  <th
+                    className={styles.thSort}
+                    onClick={() => handleSortCycle({ field: "startDate" })}
+                  >
+                    Data ↕
+                  </th>
+                  <th className={styles.thSort}>Status</th>
+                  <th className={styles.thSort}>Tipo</th>
+                </tr>
+              </thead>
+              <tbody>
+                {sortCycleOptions.cycles.map((cycle) => {
+                  const cycleType = {
+                    workTime: "Foco",
+                    shortBreakTime: "Descanso curto",
+                    longBreakTime: "Descanso longo",
+                  };
+                  return (
+                    <tr key={cycle.id}>
+                      <td>{cycle.name}</td>
+                      <td>{cycle.duration}</td>
+                      <td>{formatDate(cycle.startDate)}</td>
+                      <td>{getCycleStatus(cycle, state.activeCycle)}</td>
+                      <td>{cycleType[cycle.type]}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <div>
+            <Heading>Não há ciclos registrados</Heading>
+          </div>
+        )}
       </Container>
     </MainTemplate>
   );
--- src/types/Button/button.ts
+++ src/types/Button/button.ts
@@ -1,6 +1,6 @@
-import type { ReactNode } from "react";
+import type { ComponentProps, ReactNode } from "react";
 
-export type ButtonProps = {
+export type ButtonProps = Omit<ComponentProps<"button">, "size"> & {
   children: ReactNode;
   size?: "sm" | "md";
   variant?: "danger" | "outline" | "default";
