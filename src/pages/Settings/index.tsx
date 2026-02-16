a79a54ca18e85afcd53300dc96b6e39a470b4003
juniorbonini <juniorboninia7x@gmail.com>
2026-02-15 22:19:50 -0300

    feat(settings): add settings page layout structure

---
+++ src/pages/Settings/index.tsx
@@ -0,0 +1,42 @@
+import { Button } from "../../components/Button";
+import { Container } from "../../components/Container";
+import { Input } from "../../components/Input";
+import { MainTemplate } from "../../templates/Main";
+
+export function Settings() {
+  return (
+    <MainTemplate>
+      <Container>
+        <h1 style={{ textAlign: "center" }}>Configurações</h1>
+      </Container>
+      <Container>
+        <p style={{ textAlign: "center" }}>
+          Modifique as configurações para tempo de foco, descanso curto e
+          descanso longo.
+        </p>
+      </Container>
+      <form style={{ display: "flex", flexDirection: "column", gap: "3.2rem" }}>
+        <div>
+          <Input type="number" label="Foco (min)" id="workTime" />
+        </div>
+        <div>
+          <Input
+            type="number"
+            label="Descanso curto (min)"
+            id="shortBreakTime"
+          />
+        </div>
+        <div>
+          <Input
+            type="number"
+            label="Descanso longo (min)"
+            id="longBreakTime"
+          />
+        </div>
+        <Container>
+          <Button>Salvar</Button>
+        </Container>
+      </form>
+    </MainTemplate>
+  );
+}
